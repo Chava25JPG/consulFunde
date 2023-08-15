610f5c5b9ce7ae8f63af925de9948808eb13661f
Chava25JPG <94129167+Chava25JPG@users.noreply.github.com>
2023-08-14 22:02:07 -0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -528,17 +528,22 @@ SELECT
     IFNULL(DATE_FORMAT(MAX(dd.fecha_fotocopia_ci_vigente), '%Y-%m-%d'), '0') AS FECHA_ENVIO_UNO_DEFENSA,
 
     IFNULL(DATE_FORMAT(MAX(t.FECHA_DEFENSA), '%Y-%m-%d'), '0') AS FECHA_DEFENSA_TESIS,
-    IFNULL(MAX(t.VALOR_EVALUACION), '0') AS VALOR_EVALUACION_DEFENSA
+    IFNULL(MAX(t.VALOR_EVALUACION), '0') AS VALOR_EVALUACION_DEFENSA,
     
+    CASE
+        WHEN dt.ci_user IS NOT NULL THEN 1
+        ELSE 0
+    END AS TITULACION_ENTREGADO,
     
+    IFNULL(DATE_FORMAT(MAX(dt.fecha_fotocopia_ci_vigente), '%Y-%m-%d'), '0') AS TITULACION_FECHA_UNO
     
-
 FROM usuario u
 LEFT JOIN tesis t ON u.CI_USER = t.CI_USER AND t.COD_PROGRAMA = 'MESCER2/17'
 LEFT JOIN docs_refrenda r ON u.CI_USER = r.ci_user
 LEFT JOIN docs_defensa d ON u.CI_USER = d.ci_user
 LEFT JOIN docs_refrenda dr ON u.CI_USER = dr.ci_user
 LEFT JOIN docs_defensa dd ON u.CI_USER = dd.ci_user
+LEFT JOIN docs_titulacion dt ON u.CI_USER = dt.ci_user
 INNER JOIN matricula m ON u.CI_USER = m.CI_USER AND m.COD_PROGRA = 'MESCER2/17'
 
 GROUP BY u.CI_USER, u.NOM_USER, u.APE_PAT, u.APE_MAT  
@@ -549,12 +554,12 @@ GROUP BY u.CI_USER, u.NOM_USER, u.APE_PAT, u.APE_MAT
 
 -----------------------------------------------------------------------------------------------------
 ---------------------------------USUARIO DADO----------------------------------------------------
-
 SELECT
     u.CI_USER,
     u.NOM_USER,
     u.APE_PAT,
     u.APE_MAT,
+    p.COD_PROGRA AS CODIGO_PROGRAMA,
     CASE
         WHEN t.CI_USER IS NOT NULL THEN '1'
         ELSE '0'
@@ -570,12 +575,15 @@ SELECT
         ELSE 0
     END AS APROBADO,
     
+ 
     CASE
         WHEN MAX(t.NOMBRE_TUTOR) IS NOT NULL AND MAX(t.NOMBRE_TUTOR) <> '' THEN 1
         ELSE 0
     END AS REGISTRADO_ELABORACION_TESIS,
     
+
     CASE
+        
         WHEN (MAX(t.SUSTENTACION1) <> '' OR MAX(t.SUSTENTACION2) <> '' OR MAX(t.SUSTENTACION3) <> '') 
              AND (t.SUSTENTACION1 IS NOT NULL OR t.SUSTENTACION2 IS NOT NULL OR t.SUSTENTACION3 IS NOT NULL) THEN '1'
         ELSE '0'
@@ -586,6 +594,7 @@ SELECT
          FROM usuario usu WHERE usu.CI_USER = t.NOMBRE_TUTOR), ''
     ) AS NOMBRE_TUTOR_INICIALES,
     
+
     CASE
         WHEN MAX(r.ci_user) IS NOT NULL THEN 1
         ELSE 0
@@ -601,7 +610,14 @@ SELECT
     IFNULL(DATE_FORMAT(MAX(dd.fecha_fotocopia_ci_vigente), '%Y-%m-%d'), '0') AS FECHA_ENVIO_UNO_DEFENSA,
 
     IFNULL(DATE_FORMAT(MAX(t.FECHA_DEFENSA), '%Y-%m-%d'), '0') AS FECHA_DEFENSA_TESIS,
-    IFNULL(MAX(t.VALOR_EVALUACION), '0') AS VALOR_EVALUACION_DEFENSA
+    IFNULL(MAX(t.VALOR_EVALUACION), '0') AS VALOR_EVALUACION_DEFENSA,
+    
+    CASE
+        WHEN dt.ci_user IS NOT NULL THEN 1
+        ELSE 0
+    END AS TITULACION_ENTREGADO,
+    
+    IFNULL(DATE_FORMAT(MAX(dt.fecha_fotocopia_ci_vigente), '%Y-%m-%d'), '0') AS TITULACION_FECHA_UNO
     
 FROM usuario u
 LEFT JOIN tesis t ON u.CI_USER = t.CI_USER
@@ -609,8 +625,10 @@ LEFT JOIN docs_refrenda r ON u.CI_USER = r.ci_user
 LEFT JOIN docs_defensa d ON u.CI_USER = d.ci_user
 LEFT JOIN docs_refrenda dr ON u.CI_USER = dr.ci_user
 LEFT JOIN docs_defensa dd ON u.CI_USER = dd.ci_user
+LEFT JOIN docs_titulacion dt ON u.CI_USER = dt.ci_user
 INNER JOIN matricula m ON u.CI_USER = m.CI_USER
+INNER JOIN programa p ON m.COD_PROGRA = p.COD_PROGRA
 
-WHERE u.CI_USER = '5066309' -- Reemplaza 
+WHERE u.CI_USER = '3773113' 
 
-GROUP BY u.CI_USER, u.NOM_USER, u.APE_PAT, u.APE_MAT;
+GROUP BY u.CI_USER, u.NOM_USER, u.APE_PAT, u.APE_MAT, p.COD_PROGRA;
